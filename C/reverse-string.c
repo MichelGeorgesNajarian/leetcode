c946b20ef10bc14b7a492016213826b46c3aa7fe
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2020-02-13 15:03:54 -0500

    Update reverse-string.c

--- C/reverse-string.c
+++ C/reverse-string.c
@@ -1,15 +1,15 @@
 /*
 * https://leetcode.com/problems/reverse-string/
 * Runtime: 40 ms, faster than 98.67% of C online submissions for Reverse String.
-* Memory Usage: 13.6 MB, less than 70.00% of C online submissions for Reverse String.
+* Memory Usage: 13.5 MB, less than 90.00% of C online submissions for Reverse String.
 */
 
 void reverseString(char* s, int sSize){
     int j = sSize - 1;
     int i = 0;
-    
+    char temp;
     for (i; i < j; i++) {
-        char temp = *(s + i);
+        temp = *(s + i);
         *(s + i) = *(s + j);
         *(s + j) = temp;
         j--;
