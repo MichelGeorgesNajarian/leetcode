79fbcaa41ad9cfc158059972cf0c4d2bf2487a78
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2020-02-13 15:06:32 -0500

    Update reverse-string.cpp

--- C++/reverse-string.cpp
+++ C++/reverse-string.cpp
@@ -7,9 +7,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int j = s.size() - 1;        
+        int j = s.size() - 1;
+        char temp;
         for (int i = 0; i < j; i++) {
-            char temp = s[i];
+            temp = s[i];
             s[i] = s[j];
             s[j--] = temp;
         }
