3a5acf760b86f780efa7760b63ec033473a84aaf
MichelGeorgesNajarian <michel.georges@mgnajarian.com>
2020-02-13 12:29:48 -0500

    Reverse a string

---
+++ Java/reverse-string.java
@@ -0,0 +1,18 @@
+/*
+* https://leetcode.com/problems/reverse-string/
+* Runtime: 1 ms, faster than 99.25% of Java online submissions for Reverse String.
+* Memory Usage: 43 MB, less than 99.41% of Java online submissions for Reverse String.
+*/
+
+class Solution {
+    public void reverseString(char[] s) {
+        char[] chararray = new char[s.length];
+        int j = s.length - 1;
+        for (int i = 0; i < j; i++) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j--;
+        }
+    }
+}
